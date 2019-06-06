c836156ee91b19264fce0eed83fdd42253e290f6
XenoAmess <xenoamess@gmail.com>
2019-06-06 21:52:42 +0800

    refine PrimitiveComparator.

--- src/main/java/com/xenoamess/commonx/java/util/Arraysx.java
+++ src/main/java/com/xenoamess/commonx/java/util/Arraysx.java
@@ -2542,9 +2542,9 @@ public class Arraysx {
      * @throws ArrayIndexOutOfBoundsException if {@code fromIndex < 0} or {@code toIndex > a.length}
      */
     public static void sort(boolean[] a, int fromIndex, int toIndex) {
-        Arraysx.sort(a, fromIndex, toIndex, new Comparator<Boolean>() {
+        Arraysx.sort(a, fromIndex, toIndex, new BooleanComparator() {
             @Override
-            public int compare(Boolean o1, Boolean o2) {
+            public int compare(boolean o1, boolean o2) {
                 return o1 ? (o2 ? 0 : 1) : (o2 ? -1 : 0);
             }
         });
