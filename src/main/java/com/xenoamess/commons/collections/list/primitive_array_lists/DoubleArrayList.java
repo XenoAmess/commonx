2badb6421ff62bca24351896ceef6fac503a2712
XenoAmess <xenoamess@gmail.com>
2019-06-07 15:00:32 +0800

    documents.

--- src/main/java/com/xenoamess/commons/collections/list/primitive_array_lists/DoubleArrayList.java
+++ src/main/java/com/xenoamess/commons/collections/list/primitive_array_lists/DoubleArrayList.java
@@ -34,78 +34,27 @@ import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 /**
- * Resizable-array implementation of the {@code List} interface.  Implements
- * all optional list operations, and permits all elements, including
- * {@code null}.  In addition to implementing the {@code List} interface,
- * this class provides methods to manipulate the size of the array that is
- * used internally to store the list.  (This class is roughly equivalent to
- * {@code Vector}, except that it is unsynchronized.)
- *
- * <p>The {@code size}, {@code isEmpty}, {@code get}, {@code set},
- * {@code iterator}, and {@code listIterator} operations run in constant
- * time.  The {@code add} operation runs in <i>amortized constant time</i>,
- * that is, adding n elements requires O(n) time.  All of the other operations
- * run in linear time (roughly speaking).  The constant factor is low compared
- * to that for the {@code LinkedList} implementation.
- *
- * <p>Each {@code ArrayList} instance has a <i>capacity</i>.  The capacity is
- * the size of the array used to store the elements in the list.  It is always
- * at least as large as the list size.  As elements are added to an ArrayList,
- * its capacity grows automatically.  The details of the growth policy are not
- * specified beyond the fact that adding an element has constant amortized
- * time cost.
- *
- * <p>An application can increase the capacity of an {@code ArrayList} instance
- * before adding a large number of elements using the {@code ensureCapacity}
- * operation.  This may reduce the amount of incremental reallocation.
- *
- * <p><strong>Note that this implementation is not synchronized.</strong>
- * If multiple threads access an {@code ArrayList} instance concurrently,
- * and at least one of the threads modifies the list structurally, it
- * <i>must</i> be synchronized externally.  (A structural modification is
- * any operation that adds or deletes one or more elements, or explicitly
- * resizes the backing array; merely setting the value of an element is not
- * a structural modification.)  This is typically accomplished by
- * synchronizing on some object that naturally encapsulates the list.
+ * An resizable-array implementation of the {@code List} interface.
+ * This class aims to reduce performance issue of autoboxing and unboxing.
+ * This class is designed to be a replacement to {@code ArrayList<Double>}
+ * It shall be far faster in using.
+ * It have functions dealing with {@code Double} for being a {@code List},
+ * but nearly all of its functions have a replacement named XXXPrimitive.
+ * The basic idea is use XXXPrimitive functions whenever possible, and only use
+ * other functions when you have to.
  * <p>
- * If no such object exists, the list should be "wrapped" using the
- * {@link java.util.Collections#synchronizedList Collections.synchronizedList}
- * method.  This is best done at creation time, to prevent accidental
- * unsynchronized access to the list:<pre>
- *   List list = Collections.synchronizedList(new ArrayList(...));</pre>
- *
- * <p id="fail-fast">
- * The iterators returned by this class's {@link #iterator() iterator} and
- * {@link #listIterator(int) listIterator} methods are <em>fail-fast</em>:
- * if the list is structurally modified at any time after the iterator is
- * created, in any way except through the iterator's own
- * {@link com.xenoamess.commons.collections.list.primitive_array_lists.DoubleListIterator#remove() remove} or
- * {@link com.xenoamess.commons.collections.list.primitive_array_lists.DoubleListIterator#add(Object) add} methods,
- * the iterator will throw a
- * {@link java.util.ConcurrentModificationException}.  Thus, in the face of
- * concurrent modification, the iterator fails quickly and cleanly, rather
- * than risking arbitrary, non-deterministic behavior at an undetermined
- * time in the future.
- *
- * <p>Note that the fail-fast behavior of an iterator cannot be guaranteed
- * as it is, generally speaking, impossible to make any hard guarantees in the
- * presence of unsynchronized concurrent modification.  Fail-fast iterators
- * throw {@code ConcurrentModificationException} on a best-effort basis.
- * Therefore, it would be wrong to write a program that depended on this
- * exception for its correctness:  <i>the fail-fast behavior of iterators
- * should be used only to detect bugs.</i>
- *
- * <p>This class is a member of the
- * <a href="{@docRoot}/java.base/java/util/package-summary.html#CollectionsFramework">
- * Java Collections Framework</a>.
+ * Most of its functions are modified from {@code rrayList}, so as documents.
  *
  * @author Josh Bloch
  * @author Neal Gafter
+ * @author XenoAmess
  * @version 0.6.0
+ * @see ArrayList
  * @see Collection
  * @see List
  * @see LinkedList
  * @see Vector
+ * @see GeneratePrimitivesFromDouble
  * @since 1.2
  */
 public class DoubleArrayList extends PrimitiveArrayList<Double> {
